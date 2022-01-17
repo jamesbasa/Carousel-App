fde69f119e6d4b66698e6c390c7203117c91f547
James <jamesbasanmhs@gmail.com>
2022-01-16 20:13:33 -0800

    Add movies and shows lists

--- app/src/Carousel/CarouselPage.js
+++ app/src/Carousel/CarouselPage.js
@@ -8,13 +8,13 @@ function CarouselPage() {
     title: carousels.movies,
     urls:
       [
-        "https://image.shutterstock.com/image-photo/bright-spring-view-cameo-island-600w-1048185397.jpg",
-        "https://image.shutterstock.com/image-photo/tourism-concept-old-wooden-pier-600w-309232898.jpg",
-        "https://image.shutterstock.com/image-photo/wooden-pier-that-stretches-out-600w-1547008970.jpg",
-        "https://image.shutterstock.com/image-photo/wooden-bridge-on-cristal-clear-600w-534728500.jpg",
-        "https://image.shutterstock.com/image-photo/beach-beautiful-sea-water-maldives-600w-1294518163.jpg",
-        "https://image.shutterstock.com/image-photo/wooden-batten-bridge-juts-out-600w-765127957.jpg",
-        "https://image.shutterstock.com/image-photo/jetty-blue-sunny-serenity-600w-476916280.jpg",
+        "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_.jpg",
+        "https://flxt.tmsimg.com/assets/p7825626_p_v10_af.jpg",
+        "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
+        "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_.jpg",
+        "https://m.media-amazon.com/images/M/MV5BZGE1MDg5M2MtNTkyZS00MTY5LTg1YzUtZTlhZmM1Y2EwNmFmXkEyXkFqcGdeQXVyNjA3OTI0MDc@._V1_.jpg",
+        "https://m.media-amazon.com/images/M/MV5BMDIzODcyY2EtMmY2MC00ZWVlLTgwMzAtMjQwOWUyNmJjNTYyXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_.jpg",
+        "https://m.media-amazon.com/images/M/MV5BZWMyYzFjYTYtNTRjYi00OGExLWE2YzgtOGRmYjAxZTU3NzBiXkEyXkFqcGdeQXVyMzQ0MzA0NTM@._V1_FMjpg_UX1000_.jpg",
       ],
   };
 
@@ -22,13 +22,10 @@ function CarouselPage() {
     title: carousels.shows,
     urls:
       [
-        "https://image.shutterstock.com/image-photo/bright-spring-view-cameo-island-600w-1048185397.jpg",
-        "https://image.shutterstock.com/image-photo/tourism-concept-old-wooden-pier-600w-309232898.jpg",
-        "https://image.shutterstock.com/image-photo/wooden-pier-that-stretches-out-600w-1547008970.jpg",
-        "https://image.shutterstock.com/image-photo/wooden-bridge-on-cristal-clear-600w-534728500.jpg",
-        "https://image.shutterstock.com/image-photo/beach-beautiful-sea-water-maldives-600w-1294518163.jpg",
-        "https://image.shutterstock.com/image-photo/wooden-batten-bridge-juts-out-600w-765127957.jpg",
-        "https://image.shutterstock.com/image-photo/jetty-blue-sunny-serenity-600w-476916280.jpg",
+        "https://m.media-amazon.com/images/M/MV5BMzQ4MDMxNjExNl5BMl5BanBnXkFtZTgwOTYzODI5NTE@._V1_.jpg",
+        "https://m.media-amazon.com/images/M/MV5BM2EwMmRhMmUtMzBmMS00ZDQ3LTg4OGEtNjlkODk3ZTMxMmJlXkEyXkFqcGdeQXVyMjM5ODk1NDU@._V1_FMjpg_UX1000_.jpg",
+        "https://m.media-amazon.com/images/M/MV5BMDUzZjU5YTQtZTUyZi00NGIyLWE3YWEtZGRlOTlhMzhiNzJiXkEyXkFqcGdeQXVyNDg4NjY5OTQ@._V1_.jpg",
+        "https://m.media-amazon.com/images/M/MV5BZDcxOGI0MDYtNTc5NS00NDUzLWFkOTItNDIxZjI0OTllNTljXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_FMjpg_UX1000_.jpg",
       ],
   };
 
