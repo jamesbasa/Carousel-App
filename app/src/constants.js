685f5624a2c91ff670ac779706f36dc98ec9f128
James <jamesbasanmhs@gmail.com>
2022-01-16 19:38:43 -0800

    Refactor into constants file

---
+++ app/src/constants.js
@@ -0,0 +1,20 @@
+const paths = {
+  home: `/`,
+  about: `/about`,
+}
+
+const app = {
+  name: `Favorites Gallery`,
+  about: `About`,
+}
+
+const carousels = {
+  movies: `Movies`,
+  shows: `Shows`,
+}
+
+module.exports = {
+  paths,
+  app,
+  carousels
+}
