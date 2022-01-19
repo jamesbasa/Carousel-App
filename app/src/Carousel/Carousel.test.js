d8164b537375594ac31d92b2f97334636a1e902f
James <jamesbasanmhs@gmail.com>
2022-01-18 22:38:01 -0800

    Move app folder to root folder

--- app/src/Carousel/Carousel.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Carousel from './Carousel';
-
-test('renders learn react link', () => {
-  render(<Carousel />);
-  const linkElement = screen.getByText(/Carousel App/i);
-  expect(linkElement).toBeInTheDocument();
-});
